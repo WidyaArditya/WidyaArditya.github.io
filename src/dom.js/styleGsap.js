21a846dea9b4d5b375b7f69a9cc6c801ecf8a528
WidyaArditya <widyaarditya@gmail.com>
2024-10-01 19:47:35 +0700

    insert bootstrap into the bundle.js

--- src/dom.js/styleGsap.js
+++ src/dom.js/styleGsap.js
@@ -3,4 +3,3 @@ gsap.to('.navbar .navbar-brand', { duration: 1, delay: 1.2, text: 'Ardit' });
 gsap.from('.jumbotron img', { duration: 1, y: -100, opacity: 0, ease: 'bounce' });
 gsap.from('.navbar .nav-item', { duration: 1.3, translateX: 250, ease: 'power2' });
 gsap.from('.navbar button', { duration: 1.3, translateX: 250, ease: 'power2' });
-z;
