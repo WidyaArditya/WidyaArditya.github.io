b9bf78df0743a66108c0756f47a22bdcaf598cb4
WidyaArditya <widyaarditya@gmail.com>
2024-10-01 07:35:24 +0700

    modifide styleTilt.js

--- src/dom.js/styleTilt.js
+++ src/dom.js/styleTilt.js
@@ -1,10 +1,6 @@
-const { default: VanillaTilt } = require('vanilla-tilt');
-
-export const tilt = function () {
-  VanillaTilt.init(document.querySelectorAll('.box'), {
-    max: 25,
-    speed: 400,
-    glare: true,
-    'max-glare': 0.7,
-  });
-};
+VanillaTilt.init(document.querySelectorAll('.box'), {
+  max: 25,
+  speed: 400,
+  glare: true,
+  'max-glare': 0.7,
+});
