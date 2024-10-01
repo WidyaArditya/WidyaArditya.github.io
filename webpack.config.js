8f9c07868289ecbc14c52323ca090320ff803294
WidyaArditya <widyaarditya@gmail.com>
2024-10-01 20:38:22 +0700

    create new config webpack for development and production

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'output'),
-    filename: 'bundle.[contenthash].js',
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html',
