cd905ee5d1d59f55237720b81a16245a97851dd3
WidyaArditya <widyaarditya@gmail.com>
2024-10-03 11:38:07 +0700

    minisize main.css

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,15 @@
 const path = require('path');
+const glob = require('glob');
 const { merge } = require('webpack-merge');
 const config = require('./webpack.config');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+};
 
 module.exports = merge(config, {
   mode: 'production',
@@ -11,9 +19,32 @@ module.exports = merge(config, {
     assetModuleFilename: 'img/[name].[hash].[ext]',
     clean: true,
   },
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            // Lossless optimization with custom option
+            // Feel free to experiment with options for better result for you
+            plugins: [
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+            ],
+          },
+        },
+      }),
+    ],
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'main.[contenthash].css',
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
     }),
   ],
 });
